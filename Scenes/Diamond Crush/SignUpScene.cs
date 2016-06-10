1d795f2bf62a7bf0657c9bc195054225dfc12024
htduykhtn <htduykhtn@gmail.com>
2016-06-10 22:09:19 +0700

    Add files via upload

---
+++ Scenes/Diamond Crush/SignUpScene.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Diamond_Crush
+{
+    public partial class SignUp : Form
+    {
+        public SignUp()
+        {
+            InitializeComponent();
+            this.Username.Enabled = false;
+            this.Password.Enabled = false;
+            this.Confirm.Enabled = false;
+        }
+
+        private void username_enter_Press(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar== (char)Keys.Enter)
+            {
+
+            }
+        }
+        private void password_enter_Press(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+
+            }
+        }
+        private void confirm_enter_Press(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+
+            }
+        }
+        private void exit_Click(object sender, EventArgs e)
+        {
+            this.Close() ;
+        }
+        private void sign_up_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Online mode are not ready! Please turn back and press ESC to play offline.");
+        }
+    }
+}
